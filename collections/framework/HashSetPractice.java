48f18de14d1e654d9f44ab6671420335487165b1
janeljs <jisunlim818@gmail.com>
2021-02-03 20:08:02 +0900

    Update

--- collections/framework/HashSetPractice.java
+++ collections/framework/HashSetPractice.java
@@ -1,4 +1,19 @@
 package collections.framework;
 
+import java.util.*;
+
 public class HashSetPractice {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; set.size() < 10; i++) {
+            int num = (int) (Math.random() * 95) + 1;
+            set.add(new Integer(num));
+        }
+
+        List<Integer> list = new LinkedList<Integer>(set);
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
 }
