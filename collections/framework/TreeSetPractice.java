4219d36b0ed625f11633032b66b40d846773a530
janeljs <jisunlim818@gmail.com>
2021-02-03 20:42:38 +0900

    Upload TreeSetPractice.java

---
+++ collections/framework/TreeSetPractice.java
@@ -0,0 +1,16 @@
+package collections.framework;
+
+import java.util.*;
+
+public class TreeSetPractice {
+    public static void main(String[] args) {
+        TreeSet set = new TreeSet();
+        int[] score = {80, 90, 50, 35, 45, 65, 10, 100};
+        for (int i : score) {
+            set.add(i);
+        }
+        System.out.println(set.headSet(50));
+        System.out.println(set.tailSet(50));
+        System.out.println(set.subSet(20, 70));
+    }
+}
