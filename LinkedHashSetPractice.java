376471c68fe2868060db6fa4761b9e0647c3ec02
janeljs <jisunlim818@gmail.com>
2021-02-03 20:00:29 +0900

    Upload LinkedHashSetPractice.java

---
+++ LinkedHashSetPractice.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LinkedHashSetPractice {
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<String>();
+        set.add("a");
+        set.add("a");
+        set.add("v");
+        set.add("x");
+        set.add("x");
+        set.add("e");
+
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+}
