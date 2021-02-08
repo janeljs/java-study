b70bc69b2f3f3b96b4819bf100306d80f8900c45
janeljs <jisunlim818@gmail.com>
2021-02-09 06:04:23 +0900

    Update IO_1

--- InputOutput/IO_1.java
+++ InputOutput/IO_1.java
@@ -7,8 +7,9 @@ import java.util.Arrays;
 
 public class IO_1 {
     public static void main(String[] args) {
-        byte[] inSrc = {0,1,2,3,4,5,6,7,8,9};
+        byte[] inSrc = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         byte[] outSrc = null;
+        byte[] temp = new byte[10];
 
         ByteArrayInputStream input = null;
         ByteArrayOutputStream output = null;
@@ -18,14 +19,18 @@ public class IO_1 {
 
         int data = 0;
 
-        // read, write 사용 => 한 번에 1byte만 읽는다.
-        while((data = input.read())!=-1) {
-            output.write(data);
-        }
+//        read, write 사용 => 한 번에 1byte만 읽는다.
+//        while((data = input.read())!=-1) {
+//            output.write(data);
+//        }
+
+        input.read(temp, 0, temp.length);
+        output.write(temp, 3, 5);
 
         outSrc = output.toByteArray();
 
-        System.out.println("Input Source :" + Arrays.toString(inSrc));
-        System.out.println("Output Source :" + Arrays.toString(outSrc));
+        System.out.println("Input Source : " + Arrays.toString(inSrc));
+        System.out.println("temp : " + Arrays.toString(temp));
+        System.out.println("Output Source : " + Arrays.toString(outSrc));
     }
 }
